9e6ba4bd083e1fde55a4fbae598e6f447246f228
Rounak <62498036+Rounak-Das-02@users.noreply.github.com>
2022-01-17 18:03:38 +0530

    Changed architecture of the game

--- Battleship/BattleGround/main.py
+++ Battleship/BattleGround/main.py
@@ -1,92 +1,63 @@
 from itertools import chain
-from importlib import import_module
 import time
-
-rows = 2
-cols = 4
-
-class BattleShip :
-
-    def __init__(self, board, team_name : str, opponent_team_name):
-        self.board = board
-        self.team_name = team_name
-        self.opponent_team_name = opponent_team_name
-        self.hit_or_miss = -1
-        self.check()
+from ..example_submission import team1
+from ..example_submission import team2
 
 
-    ## Opponent attacked. Returns True if opponent has hit the player's ship, otherwise return 0
-    def got_attacked(self, x : int , y : int) -> bool:
-        
-        if x > 2 or y > 4 or x < -2 or y < -4:
-            self.hit_or_miss = -1
-            self.check()
-            return "FIRED OUT OF BATTLEZONE"
+def update_hit(board, x, y):
+    info = -1
+    if x > 2 or y > 4 or x < -2 or y < -4:
+            print("FIRED OUT OF BATTLEZONE")
 
-        if self.board[x][y] == 1:
-            self.board[x][y] = 0
-            self.hit_or_miss = 1
-            self.check()
-            return "HIT !!"
+    elif board[x][y] == 1:
+            info = 0
+            board[x][y] = info
+            print("HIT !!") 
+    else:
+        print("MISS !!")
+        info = 1 ## MISS
 
-        self.hit_or_miss = 0
-        self.check()
-        return "MISS !!"
-    
-    def check(self):
-        file_name = str(self.opponent_team_name) + ".txt"
-        with open(file_name, "w+") as file1:
-            file1.write(str(self.hit_or_miss))
+    return (board, info)
 
 
-    def check_loss(self) -> bool:
-        if all(x == 0 for x in chain(*self.board)) : 
-            return True
 
-        return False
+def game_over(board):
+    if all(x == 0 for x in chain(*board)) : 
+        return True
 
-
-player = "player"
-    
-def check():
-    return "Yo"
-
-# print((import_module("Battleship.example_submission.team1")).set_board())
-TEAM1 = "team1"
-TEAM2 = "team2"
-
-team1_module = import_module(f"Battleship.example_submission.{TEAM1}")
-team2_module = import_module(f"Battleship.example_submission.{TEAM2}")
-
-team1 = BattleShip(team1_module.set_board(), team_name = team1_module.TEAM_NAME, opponent_team_name = team2_module.TEAM_NAME)
-team2 = BattleShip(team2_module.set_board(), team_name = team2_module.TEAM_NAME, opponent_team_name = team1_module.TEAM_NAME)
+    return False
 
 
-# x,y = team1_module.attack()
+def special_check():
+    pass
 
-# print((x,y))
 
-# team2.attacked(x, y)
+team1_module = team1.BattleShip()
+team2_module = team2.BattleShip()
+
+team1_board = team1_module.set_board()
+team2_board = team2_module.set_board()
+
 
 while True:
 
     x, y = team1_module.attack()
     print("Team 1 attacked at  : " + str((x,y)))
-    res = team2.got_attacked(x, y)
+    team2_board, info = update_hit(team2_board, x, y) ## Updates the board as well as returns hit/miss
+    team1_module.hit_or_miss(x, y, info)  ## info = 0 for hit , 1 for miss, and -1 for out of range shooting
     time.sleep(1)
-    print(res)
-    if team2.check_loss():
-        print("Team 1 has won")
+    if game_over(team2_board):
+        print("Team 1 has won !!!")
         break
 
 
     x, y = team2_module.attack()
-    print(f"Team 2 attacked at : ({x, y})")
-    res = team1.got_attacked(x, y)
+    print("Team 2 attacked at  : " + str((x,y)))
+    team1_board, info = update_hit(team1_board, x, y)
+    team2_module.hit_or_miss(x, y, info)
     time.sleep(1)
-    print(res)
-    if team1.check_loss():
-        print("Team 2 has won")
+    if game_over(team1_board):
+        print("Team 2 has won !!!")
         break
 
      
--- Battleship/example_submission/team1.py
+++ Battleship/example_submission/team1.py
@@ -1,25 +1,35 @@
-from ..BattleGround import main
 import random
 
-TEAM_NAME = "EXAMPLE_TEAM_1"
+class BattleShip:
+    def __init__(self):
+        self.team_name = "Team 2"
+        self.board = board
+        self.opponent_board = opponent_board
+        self.info = -1
 
-def set_board(): ## Pre-defined function
-    board = [
-        [0 , 1 , 0, 0],
-        [1 , 1 , 0, 1]
+    def set_board(self):
+        return self.board
+
+    def attack(self):
+        x = random.randint(0,1)
+        y = random.randint(0,3)
+        print("TEAM 1 : " ,self.info)
+        return (x,y)
+
+    def hit_or_miss(self, x, y , info):
+        self.info = info
+        self.opponent_board[x][y] = info
+
+
+
+board = [
+        [1 , 1 , 0, 0],
+        [0 , 0 , 0, 0]
     ]
 
-    return board
 
+opponent_board = [
+        [-1 , -1 , -1 , -1],
+        [-1 , -1 , -1 , -1]
+    ]
 
-def attack(): ## pre-defined function
-    x = random.randint(0,1)
-    y = random.randint(0,3)
-    hit_or_miss()
-    return (x,y)
-
-def hit_or_miss():
-    file_name = str(TEAM_NAME) + ".txt"
-    with open(file_name, "r") as file1:
-        print(TEAM_NAME + " : " + file1.read())
-    pass
