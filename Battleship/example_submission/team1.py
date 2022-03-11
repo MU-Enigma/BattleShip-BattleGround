77df514448551c93fe51a54574ce2516cb0660ad
Rounak <62498036+Rounak-Das-02@users.noreply.github.com>
2022-03-11 23:44:39 +0530

    Added How to Run Doc

--- Battleship/example_submission/team1.py
+++ Battleship/example_submission/team1.py
@@ -1,5 +1,6 @@
 import random
 
+
 class BattleShip:
     def __init__(self):
         self.team_name = "Team 1"
@@ -11,38 +12,32 @@ class BattleShip:
         return self.ships
 
     def attack(self):
-        x = random.randint(0,9)
-        x = 4
-        y = random.randint(7,8)
-        y = 7
-        return (x,y)
+        x = random.randint(0, 9)
+        y = random.randint(0, 9)
+        return (x, y)
 
-    def hit_or_miss(self, x, y , info):
+    def hit_or_miss(self, x, y, info):
         self.info = info
-        ## info = 1 for miss, 0 for a hit, -1 for an out of range shooting, 2 for special move nullify. 3 for your next move to be a Hawkeye Shot
+        # info = 1 for miss, 0 for a hit, -1 for an out of range shooting, 2 for special move nullify. 3 for your next move to be a Hawkeye Shot
         if info != -1 and info == 0:
             self.opponent_board[x][y] = info
 
 
-    
 ships = [
         [5, 5, 3, 0],
         [3, 4, 5, 1],
         [2, 1, 3, 1]]
-        
-
 
 
 opponent_board = [
-        [0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0],
-        [0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0],
-        [0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0],
-        [0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0],
-        [0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0],
-        [0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0],
-        [0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0],
-        [0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0],
-        [0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0],
-        [0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0],
-    ]
-
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
+]
--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 # BattleShip-BattleGround
+
 A whole battleground to put two bot programs together to win a battle ship game
 
 Look at the [Rules](./Battleship/) for the game
+
+## How to run a test simulation!
+
+1.  git clone the repo
+2.  install all the dependencies provided in requirements.txt
+3.  Run the following command in the terminal :
+    1.  For linux users : `python3 -m Battleship`
+    2.  For Windows : `python -m Battleship`
+
+### For testing out your own algorithm/logic
+
+In the example_submission folder, replace team1.py code with your code! (You may try with team2.py as well). But, DON'T CHANGE ANY FILE NAME.
